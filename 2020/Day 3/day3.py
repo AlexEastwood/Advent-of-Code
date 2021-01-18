09090804b6f24ee6db1e04292c5d1965934b450a
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-01-18 10:15:00 +0000

    Finished day3

--- 2020/Day 3/day3.py
+++ 2020/Day 3/day3.py
@@ -1,19 +1,21 @@
-text_file = open("input.txt", "r")
-slope = text_file.readlines()
 
+with open("C:/Users/Alex - Programming/Desktop/Projects/Advent/Advent-of-Code/2020/Day 3/input.txt", "r") as file:
+    slope = [line.rstrip() for line in file]
 
-x = 3
-y = 1
-trees = []
+def ski_run(speed, drop):
+    x = 0
+    y = 0
+    trees = 0
 
-while y < len(slope):
-    try:
-        trees.append(slope[y][x])
-    except IndexError:
-        x = x - len(slope[y])
-        continue
-    x += 3
-    y += 1
+    while y < len(slope):
+        if slope[y][x % len(slope[0])] == "#":
+            trees += 1
+            
+        x += speed
+        y += drop
     
-trees_string = "".join(trees)
-print(trees_string.count("#"))
+    return trees
+
+sum = ski_run(1, 1) * ski_run(3, 1) * ski_run(5, 1) * ski_run(7, 1) * ski_run(1, 2)
+
+print(sum)
