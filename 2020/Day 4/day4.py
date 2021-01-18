50dbbb0997f0162f9048bc56b8271bb687b8edb1
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-01-18 14:52:00 +0000

    day 4 first star complete

--- 2020/Day 4/day4.py
+++ 2020/Day 4/day4.py
@@ -1,10 +1,30 @@
-import ast
-
 with open("C:/Users/Alex - Programming/Desktop/Projects/Advent/Advent-of-Code/2020/Day 4/input.txt", "r") as file:
     passports = [line.rstrip() for line in file]
     
-passport_list = []
-i = 0
+x = []
+valid = 0
+fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
+def check_fields(passport):
+    for field in fields:
+            if field not in passport:
+                return False   
+    return True
+
 for passport in passports:
-    passport = ast.literal_eval(passport)
-    print(passport)
+    if passport != "":
+        x.append(passport)
+    else:
+        y = " ".join(x)
+        if check_fields(y):
+            valid += 1
+        x = []
+
+if x != []:
+    y = " ".join(x)
+    if check_fields(y):
+        valid += 1
+    x = []
+        
+print(valid)
+    
